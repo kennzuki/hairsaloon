00ccc1716638237203be03146be236af96ccb655
kennzuki <kingsyken@gmail.com>
2024-07-15 17:48:03 +0300

    added links and navigation bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,33 @@
-function App() {
+import { Outlet } from 'react-router-dom';
+
+const App = () => {
   return (
-    <>
-      <h1 className='text-3xl font-bold underline text-center'>
-        Hello world!
-      </h1>
-    </>
+    <div className='flex flex-col gap-8 items-center p-12'>
+      <nav className='flex justify-between gap-6 place-items-center ax-w-[800px] p-8'>
+        <h1 className='text-black text-3xl font-bold'>Kenki Saloon</h1>
+        <ul className='flex justify-evenly gap-3 [a_&]:hover:scale-105'>
+          <li className=''>
+            <a href='/'>Home |</a>
+          </li>
+          <li className=''>
+            <a href='staff'>Staff |</a>
+          </li>
+          <li className=''>
+            <a href='services'>Services |</a>
+          </li>
+          <li className=''>
+            <a href='booking'>Booking |</a>
+          </li>
+          <li className=''>
+            <a href='admin'>Admin |</a>
+          </li>
+        </ul>
+      </nav>
+      <main className=''>
+        <Outlet />
+      </main>
+    </div>
   );
-}
+};
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
-import Admin from "./pages/Admin.jsx"
-import Service from "./pages/Service.jsx"
-import Staff from "./pages/Staff.jsx"
-import Home from "./pages/Home.jsx"
-import Booking from "./pages/Booking.jsx"
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Admin from './pages/Admin.jsx';
+import Services from './pages/Services.jsx';
+import Staff from './pages/Staff.jsx';
+import Home from './pages/Home.jsx';
+import Booking from './pages/Booking.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Router>
-    <Routes path='/' element={<App/>}>
-      <Route index element={<Home/>}/>
-      <Route path="staff" element={<Staff />} />
-      <Route path="services" element={<Service />} />
-      <Route path="booking" element={<Booking />} />
-      <Route path="admin" element={<Admin/>}/>
+    <Routes>
+      {/*Set the default route to render the App component containing the header along with the target page*/}
+      <Route path='/' element={<App />}>
+        {/*Set global routes with Home as the default Outlet route*/}
+        <Route index element={<Home />} />
+        <Route path='staff' element={<Staff />} />
+        <Route path='services' element={<Services />} />
+        <Route path='booking' element={<Booking />} />
+        <Route path='admin' element={<Admin />} />
+      </Route>
     </Routes>
-  </Router>,
-)
+  </Router>
+);
