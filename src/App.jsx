8c5191878407661b10e945599f916ed21aa4a2cf
kennzuki <kingsyken@gmail.com>
2024-07-15 22:31:46 +0300

    booking component extract values and react-calendar

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,11 @@ import { Outlet } from 'react-router-dom';
 const App = () => {
   return (
     <div className='flex flex-col gap-8 items-center p-12'>
-      <nav className='flex justify-between gap-6 place-items-center ax-w-[800px] p-8'>
-        <h1 className='text-black text-3xl font-bold'>Kenki Saloon</h1>
-        <ul className='flex justify-evenly gap-3 [a_&]:hover:scale-105'>
+      <nav className='flex justify-between gap-6 place-items-center ax-w-[1200px] p-8'>
+        <h1 className='text-green-500 text-3xl font-bold uppercase'>
+          Kenki Saloon
+        </h1>
+        <ul className='flex justify-evenly gap-3 [a_&]:hover:scale-105 [a_&]:text-blue-500 [a_&]:font-bold'>
           <li className=''>
             <a href='/'>Home |</a>
           </li>
--- src/pages/Booking.jsx
+++ src/pages/Booking.jsx
@@ -1,8 +1,68 @@
+import { useNavigate } from "react-router-dom"
+import Calendar from 'react-calendar'
+import { useState } from "react";
 
+const services = [
+  'Haircut',
+  'Hair Coloring',
+  'Deep Conditioning',
+  'Bridal Styling',
+  'Event Styling'
+];
+
+const stylists = [
+  'Emily Johnson',
+  'Michael Lee',
+  'Sarah Brown'
+];
+
+const handleSubmit = () => {
+  
+}
 
 const Booking = () => {
+  const [name,setName]=useState('')
+  const [email,setEmail]=useState('')
+  const [stylist,setStylist]=useState('')
+  const [service, setService] = useState('')
+  const [date,setDate]=useState('')
   return (
-    <div>Booking</div>
+    <div>
+      <h2 className="text-2xl capitalize font-bold mb-10">Book an appointment</h2>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4 border p-12 w-full">
+        {/* name */}
+        <label htmlFor="">Name</label>
+        <input type="text" className="rounded border p-4 " placeholder='Name' id="name" value={name} onChange={(e) => setName(e.target.value)} required />
+        {/* email */}
+        <label htmlFor="">Email</label>
+        <input type="text" className="rounded border p-4" placeholder='Email' id="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+        {/* calendar */}
+        <Calendar
+            onChange={setDate}
+            value={date}
+          required
+          className='text-center flex flex-col gap-4 bg-slate-300 p-4'
+        />
+        {/* current date */}
+        <p>Selected Date: {date.toLocaleDateString()}</p>
+        {/* services */}
+        <label htmlFor="">Service</label>
+        <select className="rounded border p-4" placeholder='Service' id="service" value={service} onChange={(e) => setService(e.target.value)} required>
+        <option value="" disabled>Select a service</option>
+            {services.map((service, index) => (
+              <option key={index} value={service}>{service}</option>
+            ))}
+        </select>
+        {/* stylist */}
+        <label htmlFor="">Stylist</label>
+        <select className="rounded border p-4" placeholder='Service' id="stylist" value={stylist} onChange={(e) => setStylist(e.target.value)} required>
+        <option value="" disabled>Select a stylist (optional)</option>
+            {stylists.map((stylist, index) => (
+              <option key={index} value={stylist}>{stylist}</option>
+            ))}
+        </select>
+      </form>
+    </div>
   )
 }
 
